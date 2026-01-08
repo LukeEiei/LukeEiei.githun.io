753e34651039d322bb05947ebcce5c34050de54f
LukeEiei <166229396+LukeEiei@users.noreply.github.com>
2026-01-08 14:43:48 +0700

    Delete api directory

--- api/bypluemv1/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../../config/class.php';
-$pluem = new classweb_bypluem;
-if(empty($_POST['username'] and $_POST['password'])){
-    echo json_encode(array('status'=>"error",'message'=>"กรุณากรอกข้อมูลให้ครบ"));
-}else{
-    $login = $pluem->login($_POST['username'],$_POST['password']);
-}
-?>
